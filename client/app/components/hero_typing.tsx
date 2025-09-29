049b3fd9d96d146cb7e89ab569b7e60e60f8853c
Ashbel <kashbel747@gmail.com>
2025-09-29 15:24:44 +0300

    Add hero page

---
+++ client/app/components/hero_typing.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Typewriter } from "react-simple-typewriter";
+
+export default function HeroTypingText() {
+  return (
+    <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-white">
+      <Typewriter
+        words={[
+          "I'm a Full-Stack Developer passionate about building modern, scalable, and user-friendly web applications.",
+          "Explore my projects, skills, and experience below.",
+        ]}
+        loop={0} // 0 = infinite loop, or set to 1 to type once
+        cursor
+        cursorStyle="|"
+        typeSpeed={40}
+        deleteSpeed={20}
+        delaySpeed={2000} // pause before deleting
+      />
+    </p>
+  );
+}
---
+++ client/app/pages/hero_page.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import Link from "next/link";
+import HeroTypingText from "../components/hero_typing";
+
+export default function Hero() {
+  return (
+    <section className="relative w-full bg-white dark:bg-gray-800 text-black dark:text-white">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="flex flex-col-reverse items-center gap-10 py-16 lg:flex-row lg:gap-16 lg:py-24">
+          
+          {/* Left Side - Text */}
+          <div className="flex-1 text-center lg:text-left">
+            <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white sm:text-5xl lg:text-6xl">
+              Hi, I&apos;m <span className="text-blue-600">Ashbel King&apos;ori</span>
+            </h1>
+            <HeroTypingText />
+
+            {/* Buttons */}
+            <div className="mt-8 flex flex-col items-center gap-4 sm:flex-row sm:justify-center lg:justify-start">
+              <Link
+                href="/contact"
+                className="rounded-lg bg-blue-600 px-6 py-3 text-white shadow-md transition hover:bg-blue-700"
+              >
+                Contact me
+              </Link>
+              <Link
+                href="/resume"
+                className="rounded-lg border border-blue-600 px-6 py-3 text-blue-600 transition hover:bg-blue-50"
+              >
+                View my resume
+              </Link>
+            </div>
+          </div>
+
+          {/* Right Side - Image */}
+            <div className="flex-1 flex justify-center">
+            <div className="relative h-64 w-64 sm:h-80 sm:w-80 lg:h-[420px] lg:w-[420px]">
+                <Image
+                src="/profile.jpg"
+                alt="Ashbel King'ori"
+                fill
+                className="
+                    object-bottom shadow-xl 
+                    [mask-image:url('/mask.png')] 
+                    [-webkit-mask-image:url('/mask.png')] 
+                    [mask-size:cover] 
+                    [mask-repeat:no-repeat] 
+                    [mask-position:center]"
+                priority
+                />
+            </div>
+            </div>
+
+
+        </div>
+      </div>
+    </section>
+  );
+}
