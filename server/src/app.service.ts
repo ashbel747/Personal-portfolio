e50109b57841b376f837dd0cb209d32889344a0f
Ashbel <kashbel747@gmail.com>
2025-09-29 19:43:01 +0300

    Configure app service

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return "ACCESS GRANTED(200). Wait, how did you get this far🫡? Anyways, Happy Hacking🎭"
   }
 }
